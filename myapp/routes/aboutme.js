e9a6ba64f228b12ac727b7c52f3b4f3dfcb1be33
Dhruv Pant <dhruv@gopokyo.com>
2022-10-14 13:45:41 +0530

    implementing mark down texts

--- myapp/routes/aboutme.js
+++ myapp/routes/aboutme.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET about me. */
 router.get('/', function(req, res, next) {
-  res.render('aboutme', { title: 'Dhruv Pant' });
+    res.render('aboutme');
 });
 
 module.exports = router;
---
+++ myapp/views/Markdowns/AboutMe.md
@@ -0,0 +1,3 @@
+## About Me
+
+Rendered by **marked**.
