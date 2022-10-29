734e3686c3a53cce8574e78171b76f9272d6391a
dhruv304c2 <62604491+dhruv304c2@users.noreply.github.com>
2022-10-29 12:20:26 +0530

    sorting project on index page

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,10 @@ const ProjectParser = require('../libs/projectparser');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const proProjects = ProjectParser. parseProjectGroupMarkdowns(await Project.find({type: 'Professional'}));
-  const personalProjects =  ProjectParser. parseProjectGroupMarkdowns(await Project.find({type: 'Personal'}));
-  const gamejamProjects =  ProjectParser. parseProjectGroupMarkdowns(await Project.find({type: 'Gamejam'}));
+  const proProjects = ProjectParser. parseProjectGroupMarkdowns(await Project.find({type: 'Professional'})).sort((a,b) => {return a.year - b.year;});
+  const personalProjects =  ProjectParser. parseProjectGroupMarkdowns(await Project.find({type: 'Personal'})).sort((a,b) => {return a.year - b.year;});
+  const gamejamProjects =  ProjectParser. parseProjectGroupMarkdowns(await Project.find({type: 'Gamejam'})).sort((a,b) => {return a.year - b.year;});
+
   res.render('index', { title: 'Dhruv Pant', proProjects: proProjects, personalProjects: personalProjects, gamejamProjects: gamejamProjects });
 });
 
